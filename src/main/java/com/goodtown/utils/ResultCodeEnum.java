fbd9a65efffb8261597dcabfe6add6b5dcb24801
jrcdizumi <cd15382858999@163.com>
2024-12-07 13:03:24 +0800

    #

--- src/main/java/com/goodtown/utils/ResultCodeEnum.java
+++ src/main/java/com/goodtown/utils/ResultCodeEnum.java
@@ -2,13 +2,13 @@ package com.goodtown.utils;
 
 public enum ResultCodeEnum {
     SUCCESS(200,"success"),
-    USERNAME_ERROR(501,"usernameError"),
-    PASSWORD_ERROR(503,"passwordError"),
-    NOTLOGIN(504,"notLogin"),
-    USERNAME_USED(505,"userNameUsed"),
-    PASSWORD_TOO_SHORT(506,"passwordTooShort"),
-    PASSWORD_TOO_FEW_DIGITS(507,"passwordTooFewDigits"),
-    PASSWORD_CASE_REQUIREMENT(508,"passwordCaseRequirement");
+    USERNAME_ERROR(501,"用户不存在"),
+    PASSWORD_ERROR(503,"密码错误"),
+    NOTLOGIN(504,"未登录"),
+    USERNAME_USED(505,"用户名已存在"),
+    PASSWORD_TOO_SHORT(506,"密码太短（不少于 6 位）"),
+    PASSWORD_TOO_FEW_DIGITS(507,"密码太简单（包含至少两个数字）"),
+    PASSWORD_CASE_REQUIREMENT(508,"密码太简单（包含字母且不能全为大写或小写）"),;
 
     private Integer code;
     private String message;
