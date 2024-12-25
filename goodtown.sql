1c373536fa436d184ee763049d2efd1bac6dee09
jrcdizumi <cd15382858999@163.com>
2024-12-26 00:50:11 +0800

    #

--- goodtown.sql
+++ goodtown.sql
@@ -118,11 +118,11 @@ DROP TABLE IF EXISTS `report`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `report` (
-  `monthID` varchar(6) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '统计月份',
+  `monthID` varchar(8) NOT NULL COMMENT '统计月份',
   `ptype_id` int NOT NULL COMMENT '好乡镇宣传类型标识',
   `townID` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '好乡镇标识',
-  `puser_num` int NOT NULL COMMENT '月累计宣传用户数',
-  `suser_num` int NOT NULL COMMENT '月累计助力用户数',
+  `puser_num` int NOT NULL DEFAULT '0' COMMENT '月累计宣传用户数',
+  `suser_num` int NOT NULL DEFAULT '0' COMMENT '月累计助力用户数',
   PRIMARY KEY (`monthID`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -132,6 +132,7 @@ CREATE TABLE `report` (
 --
 
 /*!40000 ALTER TABLE `report` DISABLE KEYS */;
+INSERT INTO `report` VALUES ('2024-10',2,'0',1,0),('2024-12',2,'0',7,1);
 /*!40000 ALTER TABLE `report` ENABLE KEYS */;
 
 --
@@ -147,7 +148,7 @@ CREATE TABLE `town_info` (
   `cityName` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '城市名称',
   `provinceName` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '省名称',
   PRIMARY KEY (`townID`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -155,7 +156,7 @@ CREATE TABLE `town_info` (
 --
 
 /*!40000 ALTER TABLE `town_info` DISABLE KEYS */;
-INSERT INTO `town_info` VALUES (0,'天河区','广州市','广东省'),(2,'白云区','广州市','广东省'),(3,'增城区','广州市','广东省'),(4,'从化区','广州市','广东省'),(5,'花都区','广州市','广东省'),(6,'余杭区','杭州市','浙江省'),(7,'萧山区','杭州市','浙江省'),(8,'临安区','杭州市','浙江省'),(9,'富阳区','杭州市','浙江省'),(10,'塘栖镇','杭州市','浙江省'),(11,'建邺区','南京市','江苏省'),(12,'秦淮区','南京市','江苏省'),(13,'玄武区','南京市','江苏省'),(14,'栖霞区','南京市','江苏省'),(15,'谷里镇','南京市','江苏省');
+INSERT INTO `town_info` VALUES (0,'天河区','广州市','广东省'),(2,'白云区','广州市','广东省'),(3,'增城区','广州市','广东省'),(4,'从化区','广州市','广东省'),(5,'花都区','广州市','广东省'),(6,'余杭区','杭州市','浙江省'),(7,'萧山区','杭州市','浙江省'),(8,'临安区','杭州市','浙江省'),(9,'富阳区','杭州市','浙江省'),(10,'塘栖镇','杭州市','浙江省'),(11,'建邺区','南京市','江苏省'),(12,'秦淮区','南京市','江苏省'),(13,'玄武区','南京市','江苏省'),(14,'栖霞区','南京市','江苏省'),(15,'谷里镇','南京市','江苏省'),(16,'沙坪坝区','重庆市','重庆市'),(17,'南开区','天津市','天津市'),(18,'河北区','哈尔滨市','黑龙江省'),(19,'江北区','哈尔滨市','黑龙江省'),(20,'金水区','郑州市','河南省'),(21,'管城回族区','郑州市','河南省'),(22,'莲湖区','西安市','陕西省'),(23,'未央区','西安市','陕西省'),(24,'江东区','南京市','江苏省'),(25,'秦淮区','南京市','江苏省'),(26,'锦江区','成都市','四川省'),(27,'龙泉驿区','成都市','四川省'),(28,'姑苏区','苏州市','江苏省'),(29,'吴中区','苏州市','江苏省'),(30,'渝北区','重庆市','重庆市'),(31,'九龙坡区','重庆市','重庆市'),(32,'汉阳区','武汉市','湖北省'),(33,'青山区','武汉市','湖北省'),(34,'大东区','沈阳市','辽宁省'),(35,'西岗区','大连市','辽宁省'),(36,'中原区','郑州市','河南省'),(37,'金明区','郑州市','河南省'),(38,'兴华乡','遂宁市','四川省'),(39,'和平镇','昭通市','云南省'),(40,'南山镇','宜春市','江西省'),(41,'西溪乡','梅州市','广东省'),(42,'大石镇','贺州市','广西壮族自治区'),(43,'中兴乡','恩施土家族苗族自治州','湖北省'),(44,'柳林镇','商洛市','陕西省'),(45,'东河乡','吉安市','江西省'),(46,'五湖镇','清远市','广东省'),(47,'红星乡','黔南布依族苗族自治州','贵州省'),(48,'紫云镇','保山市','云南省'),(49,'蓝田乡','渭南市','陕西省'),(50,'凤凰镇','丽江市','云南省'),(51,'松源乡','黔东南苗族侗族自治州','贵州省'),(52,'龙门镇','张家界市','湖南省'),(53,'茶山乡','延边朝鲜族自治州','吉林省'),(54,'水乡镇','泰州市','江苏省'),(55,'金岭乡','桂林市','广西壮族自治区'),(56,'碧水镇','九江市','江西省'),(57,'双江乡','大理白族自治州','云南省'),(58,'柳江镇','玉林市','广西壮族自治区'),(59,'古镇乡','盘锦市','辽宁省'),(60,'银海镇','连云港市','江苏省'),(61,'丰收乡','德阳市','四川省'),(62,'锦绣镇','泰州市','江苏省'),(63,'幸福乡','黄石市','湖北省'),(64,'瑞丰镇','驻马店市','河南省'),(65,'春光乡','六安市','安徽省'),(66,'锦程镇','承德市','河北省');
 /*!40000 ALTER TABLE `town_info` ENABLE KEYS */;
 
 --
@@ -182,7 +183,7 @@ CREATE TABLE `town_promotional` (
   KEY `f2` (`ptype_id`) USING BTREE,
   CONSTRAINT `town_promotional_ibfk_1` FOREIGN KEY (`puserid`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `town_promotional_ibfk_2` FOREIGN KEY (`ptype_id`) REFERENCES `promotional_type` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci ROW_FORMAT=COMPACT;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -190,7 +191,7 @@ CREATE TABLE `town_promotional` (
 --
 
 /*!40000 ALTER TABLE `town_promotional` DISABLE KEYS */;
-INSERT INTO `town_promotional` VALUES (1,'主题',5,0,0,'描述','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/50cc41b6-c76f-4e6c-b77b-be2f2f390dce.png,https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/b989aba8-5ce5-4f26-82d8-3822200742ab.png','2024-12-08 15:51:40',-1,'2024-12-15 12:50:42',''),(2,'`',5,0,0,'1','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/50cc41b6-c76f-4e6c-b77b-be2f2f390dce.png','2024-12-08 17:18:09',-1,'2024-12-15 12:51:41',''),(3,'测试测试测试测试',3,0,13,'测试修改','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/50cc41b6-c76f-4e6c-b77b-be2f2f390dce.png','2024-12-08 17:24:40',0,'2024-12-15 15:44:33',''),(4,'2',2,0,0,'test2','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/50cc41b6-c76f-4e6c-b77b-be2f2f390dce.png','2024-12-08 17:24:52',-1,'2024-12-15 14:32:42',''),(5,'3',4,0,0,'test3','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/b989aba8-5ce5-4f26-82d8-3822200742ab.png','2024-12-08 17:26:07',-1,'2024-12-17 21:33:25',''),(6,'3',4,0,0,'test343544ffedgde','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/b989aba8-5ce5-4f26-82d8-3822200742ab.png','2024-12-08 17:26:09',0,'2024-12-17 21:33:52',''),(7,'3',4,0,0,'test3','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/b989aba8-5ce5-4f26-82d8-3822200742ab.png','2024-12-08 17:26:11',0,'2024-12-08 17:26:11',''),(8,'3',4,0,0,'test3','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/b989aba8-5ce5-4f26-82d8-3822200742ab.png','2024-12-08 17:26:11',0,'2024-12-08 17:26:11',''),(9,'3',4,0,0,'test3','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/b989aba8-5ce5-4f26-82d8-3822200742ab.png','2024-12-08 17:26:11',0,'2024-12-08 17:26:11',''),(10,'3',4,0,0,'test3','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/b989aba8-5ce5-4f26-82d8-3822200742ab.png','2024-12-08 17:26:11',0,'2024-12-08 17:26:11','');
+INSERT INTO `town_promotional` VALUES (1,'主题',5,0,0,'描述','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/50cc41b6-c76f-4e6c-b77b-be2f2f390dce.png,https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/b989aba8-5ce5-4f26-82d8-3822200742ab.png','2024-12-08 15:51:40',-1,'2024-12-15 12:50:42',''),(2,'`',5,0,0,'1','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/50cc41b6-c76f-4e6c-b77b-be2f2f390dce.png','2024-12-08 17:18:09',-1,'2024-12-15 12:51:41',''),(3,'测试测试测试测试',3,0,13,'测试修改','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/50cc41b6-c76f-4e6c-b77b-be2f2f390dce.png','2024-12-08 17:24:40',0,'2024-12-15 15:44:33',''),(4,'2',2,0,0,'test2','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/50cc41b6-c76f-4e6c-b77b-be2f2f390dce.png','2024-12-08 17:24:52',-1,'2024-12-15 14:32:42',''),(5,'3',4,0,0,'test3','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/b989aba8-5ce5-4f26-82d8-3822200742ab.png','2024-12-08 17:26:07',-1,'2024-12-17 21:33:25',''),(6,'3',4,0,0,'test343544ffedgde','','2024-12-08 17:26:09',0,'2024-12-18 22:16:05','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/9f589dcd-5b43-42af-b44e-22d200cd7f12.mp4'),(7,'3',4,0,0,'test3','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/b989aba8-5ce5-4f26-82d8-3822200742ab.png','2024-12-08 17:26:11',0,'2024-12-08 17:26:11',''),(8,'3',4,0,0,'test3','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/b989aba8-5ce5-4f26-82d8-3822200742ab.png','2024-12-08 17:26:11',0,'2024-12-08 17:26:11',''),(9,'3',4,0,0,'test3','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/b989aba8-5ce5-4f26-82d8-3822200742ab.png','2024-12-08 17:26:11',0,'2024-12-18 22:01:13','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/c93005af-05e1-4d10-b798-16273b5c81c3.mp4'),(10,'3',4,0,0,'test3','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/b989aba8-5ce5-4f26-82d8-3822200742ab.png','2024-12-08 17:26:11',0,'2024-12-08 17:26:11',''),(11,'213123',2,0,0,'24141412321','','2024-12-25 18:17:21',0,'2024-12-25 18:17:21',''),(12,'312342',2,0,0,'421412','','2024-12-25 18:18:40',0,'2024-12-25 18:18:40',''),(13,'131243',2,0,51,'2412421','','2024-12-25 18:19:24',0,'2024-12-25 18:20:00',''),(14,'414124',2,0,0,'21412412421','','2024-10-25 18:19:24',0,'2024-10-25 18:19:24',NULL),(15,'123213',5,1438683138,40,'42242','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/5e9ae694-a446-4697-a688-2d724471688f.png','2024-12-25 21:20:16',0,'2024-12-25 21:20:16','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/c7c6401c-fdd7-47e6-b2a1-e7334db48b9b.mp4');
 /*!40000 ALTER TABLE `town_promotional` ENABLE KEYS */;
 
 --
@@ -222,7 +223,7 @@ CREATE TABLE `town_support` (
 --
 
 /*!40000 ALTER TABLE `town_support` DISABLE KEYS */;
-INSERT INTO `town_support` VALUES (524308481,0,3,'22','2224343','2024-12-17 01:16:45',1,'2024-12-17 17:41:23','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/96350db6-3dc7-4a24-9dbb-a26ec960a603.png',''),(885088258,0,3,'1','123','2024-12-17 01:08:04',3,'2024-12-17 17:13:04','',''),(1455460354,1438683138,3,'22发公告g','4reefef','2024-12-17 17:50:07',2,'2024-12-17 20:51:46','',''),(2046857218,0,3,'312323','2323323','2024-12-17 17:44:43',0,'2024-12-17 17:44:52','','');
+INSERT INTO `town_support` VALUES (524308481,0,3,'22','2224343','2024-12-17 01:16:45',1,'2024-12-17 17:41:23','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/96350db6-3dc7-4a24-9dbb-a26ec960a603.png',''),(885088258,0,3,'1','123','2024-12-17 01:08:04',3,'2024-12-17 17:13:04','',''),(1455460354,1438683138,3,'22发公告g','4reefef','2024-12-17 17:50:07',2,'2024-12-17 20:51:46','',''),(2046857218,0,3,'312323','2323323','2024-12-17 17:44:43',0,'2024-12-18 22:16:18','','https://sky-take-out-1317682162.cos.ap-beijing.myqcloud.com/goodtown/630a586b-ddba-4221-8463-1005135b7174.mp4');
 /*!40000 ALTER TABLE `town_support` ENABLE KEYS */;
 
 --
@@ -254,7 +255,7 @@ CREATE TABLE `user` (
 --
 
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (-1556008958,'RandomUser','IDCard','123456789012345678','jrcd','787ac6a6e095632819008fa9623c0e7a','13800138000','2024-12-08 15:26:50','2024-12-08 15:26:50','Normal','随机生成的用户'),(0,'jrcd','IDCard','123456789012345678','jrcd','51995c170515203fbb28df2412ce5e23','13800138000','2024-12-03 18:14:43','2024-12-17 17:48:32','Normal','随机生成的用户'),(1438683138,'cd','身份证','3432543663436','cd','51995c170515203fbb28df2412ce5e23','15382858999','2024-12-17 17:49:34','2024-12-17 17:49:34',NULL,'');
+INSERT INTO `user` VALUES (-1556008958,'RandomUser','IDCard','123456789012345678','jrcd','787ac6a6e095632819008fa9623c0e7a','13800138000','2024-12-08 15:26:50','2024-12-08 15:26:50','Normal','随机生成的用户'),(0,'jrcd','IDCard','123456789012345678','jrcd','51995c170515203fbb28df2412ce5e23','13800138000','2024-12-03 18:14:43','2024-12-17 17:48:32','Normal','随机生成的用户'),(1,'admin','IDCard','31224434343','admin','23243refegeg4141','142313','2024-12-17 17:49:34','2024-12-17 17:49:34',NULL,'admin'),(1438683138,'cd','身份证','3432543663436','cd','51995c170515203fbb28df2412ce5e23','15382858999','2024-12-17 17:49:34','2024-12-17 17:49:34',NULL,'');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 --
@@ -270,4 +271,4 @@ INSERT INTO `user` VALUES (-1556008958,'RandomUser','IDCard','123456789012345678
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-18 21:45:36
+-- Dump completed on 2024-12-26  0:49:55
